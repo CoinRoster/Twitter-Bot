a4a34428ae8258b0e6e0f23b841dd61a5c47476e
gurmitc <38087807+gurmitc@users.noreply.github.com>
2018-06-18 11:48:45 -0400

    Update golf_tweet.js

--- TwitterBot/golf_tweet.js
+++ TwitterBot/golf_tweet.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var twit = require('twit');
-var T = new twit({
-  consumer_key: 'E5eDOFhog2Dm7b3Yc75NbeSjb',
+//keys and tokens that are given after creating a twitter application
+//will be different for each handle
+var T = new twit({                                                              
+  consumer_key:         'E5eDOFhog2Dm7b3Yc75NbeSjb',                            
   consumer_secret:      'C9K4k2erEwGTwKRlmuaqyLeCeG94ayQntJsq26cigRtTlOPEWj',
   access_token:         '994232513626243072-UIVtqoCRuEDlgQhUMjMjbbsdz9Wq88A',
   access_token_secret:  'FehguX0rcJjZaoXG5BTZIqBnH2voLC9bYpDnDFLP7u42F',
@@ -18,10 +20,11 @@ request.post(
         
         if (!error && response.statusCode == 200) {
             
-            for (var i = 0; i<body['contest_report'].length; i++){
-                               
+            for (var i = 0;i < body.contest_report.length;i++){
+                console.log('Now Open: ' + body.contest_report[i].title + ' https://www.coinroster.com/contest.html?id=' + body.contest_report[i].id);
+                              
                 var tweet = {
-                status: ('Now Open: ' + body['contest_report'][i].title + ' https://www.coinroster.com/contest.html?id=' + body['contest_report'][i].id) 
+                status: ('Now Open: ' + body.contest_report[i].title + ' https://www.coinroster.com/contest.html?id=' + body.contest_report[i].id) 
                 };
                 
                 T.post('statuses/update', tweet, tweeted);  
